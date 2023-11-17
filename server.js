929645eb53ed6c637cc470d246c54909b47c4955
DOOM Guy 1485 <theboss1485@gmail.com>
2023-11-17 15:54:40 -0500

    Added Server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection.js');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'));
+
+sequelize.sync().then(() => {
+
+    console.log("sequelize synchronization successful!");
+
+    app.listen(PORT, () => {
+
+        console.log(`App listening on port ${PORT}!`);
+    })
+
+}).catch((error) => {
+
+    console.log(error)
+});
