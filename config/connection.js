e92b6a3ae8165889939e5e2af86858efe39b2531
DOOM Guy 1485 <theboss1485@gmail.com>
2023-11-30 18:01:25 -0500

    Added Database Configuration to Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,28 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+let sequelize
 
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, 
-    {
-        host: '127.0.0.1',
-        dialect: 'mysql',
-        dialectOptions: {
-            decimalNumbers: true,
-          },
-    });
+if (process.env.JAWSDB_URL) {
+
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+
+} else {
+
+    sequelize = new Sequelize(
+
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD, 
+        {
+            host: '127.0.0.1',
+            dialect: 'mysql',
+            dialectOptions: {
+                decimalNumbers: true,
+              },
+        });
+}
+
+
 
 module.exports = sequelize;
