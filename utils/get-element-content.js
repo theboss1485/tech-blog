8974986a7b046f57970b2d9616a278454ac12fce
DOOM Guy 1485 <theboss1485@gmail.com>
2024-02-28 13:56:27 -0500

    Hopefully fixed UUID issues

--- utils/get-element-content.js
+++ utils/get-element-content.js
@@ -3,15 +3,23 @@ this function gets the text of that blog post or comment so that
 it can be displayed on the page. */
 function getElementContent(res, elements, editElementId){
 
+    console.log("elements", elements);
+    console.log("editElementId", editElementId);
+
     elementInQuestion = elements.find(function(element){
 
-        if(element.id === parseInt(editElementId)){
+        if(element.id === editElementId){
 
             return element.id;
         }
     });
 
-    if(elementInQuestion !== null && elementInQuestion !== undefined){
+    console.log("element in question", elementInQuestion);
+
+    // UUIDs can't be ANDed.
+    let uuidString = elementInQuestion.toString()
+
+    if(elementInQuestion !== null && uuidString !== undefined){
 
         return elementInQuestion;
     
